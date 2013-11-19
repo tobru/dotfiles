d83032747b624a9d727800dad36c7156893db5be
Tobias Brunner <tobias@tobru.ch>
2013-11-19 16:16:48 +0100

    enhance dotfilesync

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,12 +2,16 @@
 ### sync dotfiles
 #################
 dotfilesync () {
-  cd $HOMe/.dotfiles
+  cd $HOME/.dotfiles
+  echo "running git pull in .dotfiles"
   git pull
+  echo "running git push in .dotfiles"
   git push
-  if [ -d $HOME/.private_dotfiles ]; do
+  if [ -d $HOME/.private_dotfiles ]; then
     cd $HOME/.private_dotfiles
+    echo "running git pull in .private_dotfiles"
     git pull
+    echo "running git push in .private_dotfiles"
     git push
   fi
   cd
